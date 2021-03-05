bc14573c5f86896ececb9e2bffd89c19b0edb9ca
Geraldine Paratcha <gparatcha@digiadmins-MacBook-Pro.local>
2021-03-05 14:29:59 -0300

    Add jwt token

--- src/Plugin/FrontControllerPlugin.php
+++ src/Plugin/FrontControllerPlugin.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Plus54\CloudFlareAccess\Plugin;
 use Magento\Framework\App\FrontController;
 use Magento\Framework\App\RequestInterface;
+use \Firebase\JWT\JWT;
 
 /**
  *
@@ -12,16 +13,74 @@ use Magento\Framework\App\RequestInterface;
  */
 class FrontControllerPlugin
 {
+    public const TEAM_DOMAIN = 'https://cloudflare.com';
+    public const CERTS_URL = self::TEAM_DOMAIN .'/cdn-cgi/access/certs';
+    public const ALGORITHM = 'RS256';
 
+    public function getPublicKeys(): array
+    {
+        //$this->curl->get(self::CERTS_URL);
+        //request = '';
+
+        $jwks =  ['keys' => [<<<EOD
+                -----BEGIN PUBLIC KEY-----
+                MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC8kGa1pSjbSYZVebtTRBLxBz5H
+                4i2p/llLCrEeQhta5kaQu/RnvuER4W8oDH3+3iuIYW4VQAzyqFpwuzjkDI+17t5t
+                0tyazyZ8JXw+KgXTxldMPEL95+qVhgXvwtihXC1c5oGbRlEDvDF6Sa53rcFVsYJ4
+                ehde/zUxo6UvS7UrBQIDAQAB
+                -----END PUBLIC KEY-----
+                EOD]]; //mock
+        return $jwks;
+
+    }
+
+    public function getMockPayload(){
+        $privateKey = <<<EOD
+            -----BEGIN RSA PRIVATE KEY-----
+            MIICXAIBAAKBgQC8kGa1pSjbSYZVebtTRBLxBz5H4i2p/llLCrEeQhta5kaQu/Rn
+            vuER4W8oDH3+3iuIYW4VQAzyqFpwuzjkDI+17t5t0tyazyZ8JXw+KgXTxldMPEL9
+            5+qVhgXvwtihXC1c5oGbRlEDvDF6Sa53rcFVsYJ4ehde/zUxo6UvS7UrBQIDAQAB
+            AoGAb/MXV46XxCFRxNuB8LyAtmLDgi/xRnTAlMHjSACddwkyKem8//8eZtw9fzxz
+            bWZ/1/doQOuHBGYZU8aDzzj59FZ78dyzNFoF91hbvZKkg+6wGyd/LrGVEB+Xre0J
+            Nil0GReM2AHDNZUYRv+HYJPIOrB0CRczLQsgFJ8K6aAD6F0CQQDzbpjYdx10qgK1
+            cP59UHiHjPZYC0loEsk7s+hUmT3QHerAQJMZWC11Qrn2N+ybwwNblDKv+s5qgMQ5
+            5tNoQ9IfAkEAxkyffU6ythpg/H0Ixe1I2rd0GbF05biIzO/i77Det3n4YsJVlDck
+            ZkcvY3SK2iRIL4c9yY6hlIhs+K9wXTtGWwJBAO9Dskl48mO7woPR9uD22jDpNSwe
+            k90OMepTjzSvlhjbfuPN1IdhqvSJTDychRwn1kIJ7LQZgQ8fVz9OCFZ/6qMCQGOb
+            qaGwHmUK6xzpUbbacnYrIM6nLSkXgOAwv7XXCojvY614ILTK3iXiLBOxPu5Eu13k
+            eUz9sHyD6vkgZzjtxXECQAkp4Xerf5TGfQXGXhxIX52yH+N2LtujCdkQZjXAsGdm
+            B2zNzvrlgRmgBrklMTrMYgm1NPcW+bRLGcwgW2PTvNM=
+            -----END RSA PRIVATE KEY-----
+            EOD;
+        $payload = array(
+            "iss" => "example.org",
+            "aud" => "example.com",
+            "iat" => 1356999524,
+            "nbf" => 1357000000,
+            "email" => "admin@example.com"
+        );
+        return JWT::encode($payload, $privateKey, self::ALGORITHM);
+    }
     public function beforeDispatch(FrontController $subject, RequestInterface $request)
     {
-        $cfAuthorizationCookie = $request->getCookie('CF_Authorization');
-        $cfAuthorizationCookie = null;
-        if(null === $cfAuthorizationCookie) {
-           // throw new \Exception('request forbbiden outside of cloudflare access');
-            die('Hola');
+      //  $payload = $request->getCookie('CF_Authorization');
+        $payload = $this->getMockPayload(); //TODO: mock
+
+        if (null === $payload) {
+            throw new \Exception('missing required cf authorization token');
         }
+
         //validar token con libreria jwt
+        $jwks = $this->getPublicKeys();
+        try {
+            $result = JWT::decode($payload, JWT::parseKeySet($jwks), self::ALGORITHM);
+            if ($payload['email'] !== "admin@example.com") {
+                //deny access
+                var_dump('access denied');die;
+            }
+        } catch (\Exception $e) {
+            die('oops');
+        }
     }
 }
 
