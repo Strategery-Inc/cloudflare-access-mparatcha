f4e64f24e676a71361386a4609f249b87adbe634
gparatcha <gparatcha@plus54.com>
2021-04-06 09:55:03 -0300

    add setup to TokenValidationTest class

--- tests/TokenValidatorTest.php
+++ tests/TokenValidatorTest.php
@@ -15,20 +15,31 @@ final class TokenValidatorTest extends TestCase
 
     public function setUp(): void
     {
+        //$jsonBody = '{"foo": "bar"}';
+        //$url = 'http://example.com';
+        $possibleApiResponses = [
+            'http://example.com' => ['{"foo": "bar"}', ["foo" => "bar"]]
+        ];
+        $state = new \stdClass();
+        $state->url = null;
         $this->json = $this->getMockBuilder(Json::class)->getMock();
         $this->curlClient = $this->getMockBuilder(Curl::class)->getMock();
+        $this->curlClient->expects($this->any())->method('get')->with($this->callback(
+            fn($arg) => $state->url = $arg
+        ));
+        $this->curlClient->expects($this->any())->method('getBody')
+            ->willReturnCallback(fn () => $possibleApiResponses[$state->url][0]);
+            //->willReturn($jsonBody);
+        $this->json->expects($this->any())->method('unserialize')->with($possibleApiResponses[$state->url][0])
+            ->willReturn($possibleApiResponses[$state->url][1]);
+            //->with($jsonBody);
     }
-
+//Gabi: run test
     public function testGetPublicKeys(): void
     {
-        $url = 'http://example.com';
-        $this->curlClient->expects($this->once())->method('get')->with($url);
-        $this->curlClient->expects($this->once())->method('getBody');
-        $this->json->expects($this->once())->method('unserialize')->with($this->isType('string'));
         $tokenValidator = new TokenValidator($this->json, $this->curlClient);
-
-        $res = $tokenValidator->getPublicKeys($url);
-        $this->assertArrayHasKey('kid', $res);
-        //$this->assertSame(expected: )
+        $res = $tokenValidator->getPublicKeys('http://example.com');
+        $this->assertIsArray($res);
+        $this->assertArrayHasKey('foo', $res);
     }
 }
